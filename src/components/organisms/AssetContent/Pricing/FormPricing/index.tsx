47bf1c140e7eb10140a9423e0134a610ffcc6322
Sudhanshu <shukla.sudhanshu@yahoo.com>
2022-02-22 12:32:56 +0530

    fixed footer

--- src/components/organisms/AssetContent/Pricing/FormPricing/index.tsx
+++ src/components/organisms/AssetContent/Pricing/FormPricing/index.tsx
@@ -66,7 +66,7 @@ export default function FormPricing({
           content: <Fixed content={content.fixed} ddo={ddo} />
         }
       : undefined,
-    appConfig.allowDynamicPricing === 'true'
+    appConfig.allowDynamicPricing === 'false'
       ? {
           title: content.dynamic.title,
           content: <Dynamic content={content.dynamic} ddo={ddo} />
